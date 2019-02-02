4abc71f815f269c3e831f7eff179fc9ae8c680fb
ychen_cloud <victoriuscv.u1@gmail.com>
2019-02-02 18:04:11 +0100

    Corrección config gruntfile

--- gruntfile/Gruntfile.js
+++ gruntfile/Gruntfile.js
@@ -2,27 +2,27 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-	pkg: grunt.file.readJSON('package.json'),
+		pkg: grunt.file.readJSON('package.json'),
+		sass: {																// Task
+		  dist: {															// Target
+				options: {												// Target options
+				  style: 'expanded'
+				},
+				files: {													// Dictionary of files
+				  '../public/gold/styles/style.css': '../public/gold/styles/process/main.scss',	// 'destination': 'source'
+				}
+		  }
+		},
+		watch: {
+		  scripts: {
+				options: { livereload: true },
+				files: [
+					'../public/index.html',
+					'../public/gold/styles/style.css',
+				],
+		  }
+		},
 
-	sass: {																// Task
-	  dist: {															// Target
-			options: {												// Target options
-			  style: 'expanded'
-			},
-			files: {													// Dictionary of files
-			  '../public/gold/styles/style.css': '../public/gold/styles/process/main.scss',	// 'destination': 'source'
-			}
-	  }
-	}
-	watch: {
-	  scripts: {
-			options: { livereload: true },
-			files: {
-				'../public/index.html',
-				'../public/gold/styles/style.css'
-			}
-	  }
-	}
   });
 
   grunt.loadNpmTasks('grunt-contrib-watch');
