f37c4a798b0299a8537a2422f9261b47654c0276
ychen_cloud <victoriuscv.u1@gmail.com>
2019-02-03 17:50:15 +0100

    Scripts separado del html

--- public/gold/scripts/script.js
+++ public/gold/scripts/script.js
@@ -1,3 +1,22 @@
-(function(){
- // some code…
-})();
+$( function() {
+	/* Habilita arrastable de cada enlace */
+	$( "#sortable1, #sortable2, #sortable3, #sortable4" ).sortable( {
+		connectWith: ".connectedSortable"
+	} ).disableSelection();
+	/* Catch elements when change position */
+	$( '#sortable1' ).sortable( {
+		update: function( event, ui ) {
+			var data = $( this ).sortable( 'toArray' );
+			console.log( data );
+			console.log( data[ 0 ] );
+		}
+	} );
+	$( '#sortable2' ).sortable( {
+		// axis: 'y', // sólo posibilita movimiento entre ese eje
+		update: function( event, ui ) {
+			var data = $( this ).sortable( 'toArray' );
+			console.log( data );
+			console.log( data[ 0 ] );
+		}
+	} );
+} );
